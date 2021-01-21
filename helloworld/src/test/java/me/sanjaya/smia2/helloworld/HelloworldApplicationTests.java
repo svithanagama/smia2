d9a186c8a34487cd8817d24de45ccde4cddcd964
Sanjaya Vithanagama <svithanagama@gmail.com>
2021-01-21 21:19:31 +1100

    MS-1: Hello world app for SMIA 2nd ed chapter 1.

---
+++ helloworld/src/test/java/me/sanjaya/smia2/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sanjaya.smia2.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
